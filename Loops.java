0b582f2dae27cff4418882a81d08f8c97462742f
freddrapari <freddrapari95@gmail.com>
2024-05-23 08:52:41 +0300

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.vu.loops;
+
+/**
+ *
+ * @author hp
+ */
+public class Loops {
+    // do loops
+    //do while loops
+    //while loops
+    //foreach loops
+    
+    for(declaration and initialization;range;increment/decrement){
+    
+    //}
+}
+
+    public static void main(String[] args) {
+        System.out.println("even numbers between 10 and 40");
+        for(int a=10; a=<40;a++);{
+        System.out.println(a+",");
+    }
+}
